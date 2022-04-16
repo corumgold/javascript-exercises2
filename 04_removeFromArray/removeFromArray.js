36595d0fb08df29012705736a034bea9a40ce932
Corum Gold <goldcorum@gmail.com>
2022-04-16 18:41:13 -0400

    Finish

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,12 @@
 function removeFromArray(...args) {
     let array = args[0]
     let newArray = [];
-    
+    array.forEach(element => {
+        if (array.includes(element)) {
+            newArray.push(element)
+        }
+        return newArray
+    }
 };
 // Do not edit below this line
 module.exports = removeFromArray;
