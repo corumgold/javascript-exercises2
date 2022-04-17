09755b5d589c0b8b9f30b5ea6be23ab7654bdb45
Corum Gold <goldcorum@gmail.com>
2022-04-17 13:18:41 -0400

    Finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function (firstNum, lastNum) {
+    let total = 0;
+    if (firstNum < 0 || lastNum < 0) {
+        return 'ERROR';
+    } else if (typeof firstNum != 'number' || typeof lastNum != 'number') {
+        return 'ERROR';
+    } else if (firstNum < lastNum) {
+        for (let i = firstNum; i <= lastNum; i++) {
+            total += i;
+        } return total;
+    } else if (firstNum > lastNum) {
+        for (let i = lastNum; i <= firstNum; i++) {
+            total += i;
+        } return total;
+    }
 
 };
 
